52b5e9e99a3c03878dd88be4301923ebbdb640d6
Neil Raphael Ramos <neilraphael.ramos4@gmail.com>
2024-11-26 20:52:40 +0800

    Adding the Nav, User Info and Reponsive of Client Form

--- ADT313-movieproject/admin-dashboard-neilraphael-ramos-bsit3a/src/pages/Client/Client.jsx
+++ ADT313-movieproject/admin-dashboard-neilraphael-ramos-bsit3a/src/pages/Client/Client.jsx
@@ -1,7 +1,14 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useContext, useCallback } from 'react'
 import { Outlet, useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignOutAlt, faFilm, faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { AuthContext } from '../../utils/context/AuthContext';
 import './Client.css'
 function Client() {
+    const { auth } = useContext(AuthContext);
+    const { clearAuthData } = useContext(AuthContext);
+    const navigate = useNavigate();
+
     useEffect(() => {
         const header = document.querySelector('header');
         const footer = document.querySelector('footer');
@@ -27,6 +34,11 @@ function Client() {
         };
     }, []);
 
+    const handleLogout = useCallback(() => {
+        clearAuthData();
+        navigate('/');
+    }, [navigate, clearAuthData]);
+
     return (
         <>
             <main className="box">
@@ -45,17 +57,44 @@ function Client() {
                     <Outlet />
                 </article>
 
-                <aside>
-                    Sidebar
+                <aside className='sidebar'>
+                    <div className='container-user-info'>
+                        <div className='User-Info'>
+                            <FontAwesomeIcon icon={faUserCircle} style={{ fontSize: '90px', color: 'white' }} />
+                            <span className="user-info-data">
+                                <h1 className="name-user">{auth.user.firstName}</h1>
+                                <p className="role-user">Role as: <strong>{auth.user.role}</strong></p>
+                            </span>
+                        </div>
+                    </div>
+                    <hr></hr>
+                    <ul className='nav'>
+                        <div className="Movies-user">
+                            <div style={{ fontSize: '24px', color: 'white' }}>
+                                <center>
+                                    <FontAwesomeIcon icon={faFilm} style={{ fontSize: '24px', color: 'white' }} />
+                                    <strong className='spacing-text'>Movies</strong>
+                                </center>
+                            </div>
+                        </div>
+                        <div className="logout-user">
+                            <div onClick={handleLogout} style={{ fontSize: '24px', color: 'white' }}>
+                                <center>
+                                    <FontAwesomeIcon icon={faSignOutAlt} style={{ fontSize: '24px', color: 'white' }} />
+                                    <strong className='spacing-text'>Logout</strong>
+                                </center>
+                            </div>
+                        </div>
+                    </ul>
                 </aside>
 
                 <footer>
                     <div className="text-rights">
-                        <p>
+                        <p className='size-font'>
                             &copy; 2024 MovieWebDB - All rights reserved.
                             Data provided by <a className='link-color' href="https://www.themoviedb.org" target="_blank">(TMDb)</a>
                         </p>
-                        <p>
+                        <p className='size-font'>
                             Developed and Created by Neil Raphael M. Ramos from BSIT - 3A
                         </p>
                     </div>
